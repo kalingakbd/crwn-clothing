79fc2752f5d0883ca7c5a5fdee4023ecdf719d49
Kalinga Dissanayake <kalingakbd@gmail.com>
2020-06-28 13:36:21 -0700

    changes to break down home page + other changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './pages/HomePage'
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div>
+      <HomePage/>
     </div>
   );
 }
