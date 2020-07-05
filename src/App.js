36719572b2de30234581db57ae1c46ac3d65da87
Kalinga Dissanayake <kalingakbd@gmail.com>
2020-07-04 18:48:54 -0700

    commit header

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import HomePage from './pages/HomePage'
 import ShopPage from './pages/ShopPage'
 import {Route, Switch} from 'react-router-dom';
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
+        <Header/>
         <Switch>
             <Route exact path='/shop' component={ShopPage} />
             <Route exact path='/' component={HomePage} />
